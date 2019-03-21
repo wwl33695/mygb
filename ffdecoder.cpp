1fb57aed464402b3efeae9f2d366bf45d2132f3d
wwl33695 <wwl33695@126.com>
2019-03-21 15:48:32 +0800

    modify

---
+++ ffdecoder.cpp
@@ -0,0 +1,324 @@
+#include "ffdecoder.h"
+#include <unistd.h>
+#include <stdio.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+//#include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
+//#include <libavfilter/avfilter.h>
+#include <libavutil/avutil.h>
+#include <libavutil/error.h>
+#include <libswscale/swscale.h>
+//#include <libavutil/pixdesc.h>
+}
+
+FFDecoder::FFDecoder()
+{
+    m_ifmt_ctx = NULL;
+    m_avctx = NULL;
+    m_swsContext = NULL;
+    m_video_index = -1;
+    m_stop = false;
+
+    m_width = -1;
+    m_height = -1;
+
+    av_register_all();
+    avformat_network_init();
+}
+
+FFDecoder::~FFDecoder()
+{
+    m_stop = true;
+
+    if (m_avctx != NULL) {
+        avcodec_free_context(&m_avctx);
+        m_avctx = NULL;
+    }
+
+    if (m_ifmt_ctx != NULL) {
+        avformat_close_input(&m_ifmt_ctx);
+        m_ifmt_ctx = NULL;
+    }
+}
+
+void FFDecoder::Stop()
+{
+    m_stop = true;
+    m_read.join();
+    m_decode.join();
+}
+
+bool FFDecoder::OpenUrl(const char* url, int gpu)
+{
+    char    errbuf[64];
+    int ret = avformat_open_input(&m_ifmt_ctx, url, 0, NULL);
+    if (ret < 0) {
+        printf("avformat_open_input '%s' (error '%s')\n", url, av_make_error_string(errbuf, sizeof(errbuf), ret));
+        return false;
+    }
+
+    ret = avformat_find_stream_info(m_ifmt_ctx, NULL);
+    if (ret < 0) {
+        printf("avformat_find_stream_info (error '%s')\n", av_make_error_string(errbuf, sizeof(errbuf), ret));
+        return false;
+    }
+
+    for (unsigned int i = 0; i < m_ifmt_ctx->nb_streams; i++) {
+        av_dump_format(m_ifmt_ctx, i, url, 0);                                // dump information
+        AVStream *st = m_ifmt_ctx->streams[i];
+        switch(st->codecpar->codec_type) {
+        case AVMEDIA_TYPE_AUDIO:
+            break;
+        case AVMEDIA_TYPE_VIDEO:
+            m_video_index = i;
+            GetCodec(AV_CODEC_ID_H264, gpu);
+            break;
+        default: break;
+        }
+    }
+    if (m_avctx == NULL) {
+        printf("No H.264 video stream in the input file\n");
+        return false;
+    }
+
+    m_read = std::thread(ReadThread, this);
+
+    return true;
+}
+
+bool FFDecoder::GetInfo(int *width, int *height)
+{
+    if (m_avctx == NULL) {
+        return false;
+    }
+
+    *width = m_width;
+    *height = m_height;
+    return true;
+}
+
+bool FFDecoder::SetPacketData(uint8_t *data, int length)
+{
+    if( !data || length <= 0 )
+        return false;
+
+    AVPacket *pkt = av_packet_alloc();
+    av_new_packet(pkt, length);
+    memcpy(pkt->data, data, length);
+    std::lock_guard<std::mutex> lock(m_mtx);
+    this->m_pkts.push_back(pkt);
+
+    return true;
+}
+
+bool FFDecoder::GetRGBData(uint8_t *data, int width, int height)
+{
+    AVFrame *frame = NULL;
+//    while (frame == NULL) 
+    {
+        std::lock_guard<std::mutex> lock(m_mtx_frames);
+        if (!m_frames.empty()) {
+//            printf("FFDecoder::GetRGBData:framesize = %d \n", m_frames.size());
+            frame = m_frames.front();
+            m_frames.pop_front();
+//            break;
+        }
+    }
+
+    if (frame == NULL) {
+        return false;
+    }
+
+    if (frame->format != AV_PIX_FMT_BGR24 || frame->width != width || frame->height != height) {
+        m_swsContext = sws_getCachedContext(m_swsContext,
+                                            frame->width, frame->height, (AVPixelFormat)frame->format,
+                                            width, height, AV_PIX_FMT_BGR24,
+                                            0, NULL, NULL, NULL);
+        uint8_t *pixels[4] = { data };
+        int pitch[4] = { width * 3 };
+        sws_scale(m_swsContext, (const uint8_t *const *)frame->data, frame->linesize,
+                  0, frame->height, pixels, pitch);
+
+        if (frame->pts == 93600) {
+            FILE *file = fopen("1.rgb", "w+");
+            fwrite(data, width * height * 3, 1, file);
+            fclose(file);
+        }
+
+    } else {
+
+    }
+
+    av_frame_free(&frame);
+    return true;
+}
+
+bool FFDecoder::GetCodec(int codec_id, int gpu)
+{
+    int ret = 0;
+    AVCodec *codec = NULL;
+    const char *forced_codec_name = NULL;
+    AVDictionary *opts = NULL;
+
+    AVCodecContext *avctx = avcodec_alloc_context3(NULL);
+    if (!avctx) {
+        printf("avcodec_alloc_context3 error\n");
+        return false;
+    }
+
+    codec_id = AV_CODEC_ID_H264;
+    if (codec_id == AV_CODEC_ID_MJPEG) {
+        forced_codec_name = "mjpeg_cuvid";
+    } else if (codec_id == AV_CODEC_ID_H264) {
+        forced_codec_name = "h264_cuvid";
+    } else if (codec_id == AV_CODEC_ID_HEVC) {
+        forced_codec_name = "hevc_cuvid";
+    } else if (codec_id == AV_CODEC_ID_MPEG4) {
+        forced_codec_name = "mpeg4_cuvid";
+    }
+
+    if (forced_codec_name) {
+        codec = avcodec_find_decoder_by_name(forced_codec_name);
+        if (codec) {
+            char buf[32];
+            snprintf(buf, 32, "%d", gpu);
+            av_dict_set(&opts, "gpu", buf, 0);
+            ret = avcodec_open2(avctx, codec, &opts);
+            if (ret < 0) {
+                char errbuf[64];
+                printf("avcodec_open2 %s error '%s'\n", forced_codec_name, av_make_error_string(errbuf, sizeof(errbuf), ret));
+                codec = NULL;
+            }
+        }
+    }
+    if (!codec) {
+        codec = avcodec_find_decoder((AVCodecID)codec_id);
+    }
+
+    if (!codec) {
+        printf("avcodec_find_decoder error\n");
+        goto fail;
+    }
+
+    avctx->codec_id = codec->id;
+
+    ret = avcodec_open2(avctx, codec, NULL);
+    if (ret < 0) {
+        char errbuf[64];
+        printf("avcodec_open2 error '%s'\n", av_make_error_string(errbuf, sizeof(errbuf), ret));
+        goto fail;
+    }
+    m_avctx = avctx;
+    m_decode = std::thread(DecodeThread, this);
+
+    goto out;
+
+fail:
+    avcodec_free_context(&avctx);
+    avctx = NULL;
+    return false;
+
+out:
+    av_dict_free(&opts);
+    return true;
+}
+
+void FFDecoder::ReadThread(FFDecoder *that)
+{
+    AVPacket *pkt = NULL;
+    while (!that->m_stop) {
+        if (pkt == NULL) {
+            pkt = new AVPacket;
+        }
+
+        if (pkt == NULL) {
+            printf("new AVPacket error, exit thread!!!");
+            break;
+        }
+
+        int ret = av_read_frame(that->m_ifmt_ctx, pkt);
+        if (ret < 0) {
+            char errbuf[64];
+            printf("av_read_frame error '%s', exit thread!!!\n", av_make_error_string(errbuf, sizeof(errbuf), ret));
+            break;
+        } else if (ret == AVERROR(EAGAIN)) {
+            usleep(10 * 1000);
+            continue;
+        } else if (pkt->stream_index != that->m_video_index) {
+            av_packet_unref(pkt);
+        } else {
+            that->SetPacketData(pkt->data, pkt->size);
+            
+            av_packet_unref(pkt);
+            delete pkt;
+
+//            std::lock_guard<std::mutex> lock(that->m_mtx);
+//            that->m_pkts.push_back(pkt);
+            pkt = NULL;
+        }
+    }
+
+    if (pkt) {
+        delete pkt;
+        pkt = NULL;
+    }
+}
+
+void FFDecoder::DecodeThread(FFDecoder *that)
+{
+    pthread_setname_np(pthread_self(), "video_decode");
+    AVFrame *frame = av_frame_alloc();
+    while (!that->m_stop) 
+    {
+        AVPacket *pkt = NULL;
+        {
+            std::lock_guard<std::mutex> lock(that->m_mtx);
+            if (!that->m_pkts.empty()) 
+            {
+                pkt = that->m_pkts.front();
+                that->m_pkts.pop_front();
+//                printf("FFDecoder::DecodeThread:packetsize = %d \n", that->m_pkts.size());
+            }
+        }
+
+        if( !pkt )
+        {
+            usleep(40 * 1000);
+            continue;
+        }
+
+        int got_frame = 0;
+        int ret = avcodec_decode_video2(that->m_avctx, frame, &got_frame, pkt);
+        if (ret < 0) 
+        {
+            fprintf(stderr, "Error decoding video frame \n");
+//            break;
+        }
+        else if( got_frame )
+        {
+            {
+                std::lock_guard<std::mutex> lock(that->m_mtx_frames);
+                that->m_width = frame->width;
+                that->m_height = frame->height;
+                that->m_frames.push_back(frame);
+            }
+            frame = av_frame_alloc();
+            if (frame == NULL) {
+                printf("av_frame_alloc error!!!");
+                break;
+            }
+        }
+        if ( pkt && pkt->data )
+        {        
+            av_packet_free(&pkt);
+/*
+            delete []pkt->data;
+            delete pkt;
+*/
+        }
+
+        usleep(10 * 1000);
+    }
+}
---
+++ gb28181.cpp
@@ -0,0 +1,374 @@
+
+#ifndef WIN32
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+#else
+	#include <winsock2.h>
+
+	#pragma comment(lib, "ws2_32.lib")
+	#pragma comment(lib, "mxml1.lib")
+	#pragma comment(lib, "eXosip.lib")
+	#pragma comment(lib, "libcares.lib")
+	#pragma comment(lib, "osip2.lib")
+
+	//Dnsapi.lib;Iphlpapi.lib;ws2_32.lib;eXosip.lib;osip2.lib;osipparser2.lib;Qwave.lib;libcares.lib;delayimp.lib;
+	//忽略 libcmt.lib默认库
+	#pragma comment(lib, "Dnsapi.lib")
+	#pragma comment(lib, "Iphlpapi.lib")
+	#pragma comment(lib, "osipparser2.lib")
+	#pragma comment(lib, "Qwave.lib")
+	#pragma comment(lib, "delayimp.lib")
+
+	#ifdef DEBUG
+	#pragma comment(lib, "jrtplib_d.lib") 
+	#pragma comment(lib,"jthread_d.lib")
+	#pragma comment(lib,"WS2_32.lib")
+	#else
+	#pragma comment(lib, "jrtplib.lib") 
+	#pragma comment(lib,"jthread.lib")
+	#pragma comment(lib,"WS2_32.lib")
+	#endif
+
+#endif // WIN32
+
+#include "mysip.h"
+
+#define PS_BUF_SIZE         (1024*1024*4)
+
+int ParsePsStream(char* psBuf, uint32_t &psLen, char* rtpPayload, uint32_t rtpPayloadLength, CameraParams *p)
+{
+	static uint32_t cnt = 0;
+	if (rtpPayloadLength <= 0)
+	{
+		perror("recvfrom() long");
+		return -1;
+	}
+
+	char* ptr = psBuf + psLen;			//最新数据的头
+	if (psLen + rtpPayloadLength < PS_BUF_SIZE)
+	{
+		memcpy(ptr, rtpPayload, rtpPayloadLength);
+	}
+	else
+	{
+		printf("psBuf memory overflow, %d\n", psLen + rtpPayloadLength);
+		psLen = 0;
+		return -1;
+	}
+
+	//视频流解析
+	if (psLen > 0 && 
+		ptr[0] == 0x00 && ptr[1] == 0x00 && ptr[2] == 0x01 && ptr[3] == 0xffffffBA)
+	{
+		if (cnt % 25 == 0)
+		{
+			p->status = 1;
+		}
+
+		p->parser.Parse(psBuf, psLen);
+	
+		char *packet = NULL;
+		uint32_t packetsize = 0;
+		if( p->parser.getpacket(&packet, &packetsize) >= 0 )
+		{
+			if( p->fpH264 )
+				fwrite(packet, 1, packetsize, p->fpH264);
+			else
+				p->decoder.SetPacketData((uint8_t*)packet, packetsize);
+		}
+
+		memcpy(psBuf, ptr, rtpPayloadLength);
+		psLen = 0;
+		cnt++;
+	}
+	psLen += rtpPayloadLength;
+
+	return 0;
+}
+
+int getrtpsession(jrtplib::RTPSession &sess, int &rtpport)
+{
+	uint16_t portbase;
+	int i, num;
+
+	jrtplib::RTPUDPv4TransmissionParams transparams;
+	jrtplib::RTPSessionParams sessparams;
+
+	sessparams.SetOwnTimestampUnit(1.0 / 9000.0);
+	sessparams.SetAcceptOwnPackets(true);
+
+	for (uint16_t i = 6000; i < 60000; i+=2)
+	{
+		transparams.SetPortbase(i);
+		int status = sess.Create(sessparams, &transparams);
+		if (status >= 0)
+		{
+			rtpport = i;
+			return 0;
+		}
+		printf("getrtpsession error: %d, %s \n", status, jrtplib::RTPGetErrorString(status).c_str() );
+		sess.Destroy();
+	}
+	return -1;
+}
+
+int jrtplib_rtp_recv_thread(void* arg)
+{
+    pthread_setname_np(pthread_self(), "recv_thread");
+
+	//获取相机参数
+	CameraParams *p = (CameraParams *)arg;
+
+	char *psBuf = (char *)malloc(PS_BUF_SIZE);
+	if (psBuf == NULL)
+	{
+		//APP_ERR("malloc failed");
+		printf("malloc failed \n");
+		return -1;
+	}
+	memset(psBuf, 0, PS_BUF_SIZE);
+	uint32_t psLen = 0;
+
+#ifdef WIN32
+	WSADATA dat;
+	WSAStartup(MAKEWORD(2, 2), &dat);
+#endif // WIN32
+
+	//写入视频文件
+	//获取当前程序路径
+	char filename[MAX_PATH];
+	std::string strPath = p->sipId;
+	snprintf(filename, MAX_PATH, "%s.264", strPath.c_str());
+	if( p->writefile )
+		p->fpH264 = fopen(filename, "wb");
+
+	if (p->fpH264 == NULL)
+	{
+		printf("fopen %s failed \n", filename);
+	}
+
+	uint32_t last_ts = 0;
+	//开始接收流包
+	while (p->running)
+	{
+#ifndef RTP_SUPPORT_THREAD
+		p->sess.WaitForIncomingData(jrtplib::RTPTime(1, 1000));
+
+		if( p->sess.Poll() < 0 )
+		{
+			printf("sess.Poll() error \n");
+			break;
+		}
+#endif // RTP_SUPPORT_THREAD
+
+		p->sess.BeginDataAccess();
+		if (p->sess.GotoFirstSourceWithData())
+		{
+			do{
+				jrtplib::RTPPacket *pack = p->sess.GetNextPacket();
+
+				if( !pack )
+				{
+					printf("jrtplib_rtp_recv_thread:packet is null \n");
+					std::this_thread::sleep_for(std::chrono::milliseconds(1));
+				}
+				else
+				{
+//					printf("Got packet! %d \n", pack->GetPayloadLength());
+
+					uint32_t ts = pack->GetTimestamp();
+					if (ts >= last_ts || abs(int(ts - last_ts))/90000 >= 3600 )
+					{
+						ParsePsStream(psBuf, psLen, (char*)pack->GetPayloadData(), pack->GetPayloadLength(), p);
+						last_ts = ts;
+					}
+
+					//写入文件
+//					fwrite(pack->GetPayloadData(), 1, pack->GetPayloadLength(), p->fpH264);
+					p->sess.DeletePacket(pack);
+				}
+			} while (p->sess.GotoNextSourceWithData());
+		}
+		p->sess.EndDataAccess();
+
+//		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+
+	p->sess.BYEDestroy(jrtplib::RTPTime(0, 1000), 0, 0);
+
+#ifdef WIN32
+	WSACleanup();
+#endif // WIN32
+
+	p->decoder.Stop();
+
+	if( p->fpH264 )
+	{
+		fclose(p->fpH264);
+		p->fpH264 = NULL;
+	}
+
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void *gb28181_init(char* localip, int localport, char *localsipid)
+{
+	struct eXosip_t *sipctx = mysip_init(localport);
+	if (!sipctx)
+		return NULL;
+
+	liveVideoStreamParams *inst = new liveVideoStreamParams;
+	if (!inst)
+	{
+		mysip_uninit(sipctx);
+		return NULL;
+	}
+
+	inst->gb28181Param.running = 1;
+	inst->gb28181Param.eCtx = sipctx;
+	strcpy(inst->gb28181Param.localSipId, localsipid);
+	strcpy(inst->gb28181Param.localIpAddr, localip);
+	inst->gb28181Param.localSipPort = localport;
+	
+	inst->gb28181Param.msgthread = std::thread(MsgThreadProc, inst);
+
+	return inst;
+}
+
+int gb28181_startstream(void *handle, char* deviceip)
+{
+	if (!handle || !deviceip)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+
+	CameraParams *param = NULL;
+	if (getdeviceinfo(inst, deviceip, &param) < 0)
+	{
+		printf("getdeviceinfo error \n");
+		return -1;
+	}
+
+	if( !param->decoder.GetCodec(27, 0) )
+	{
+		printf("GetCodec error \n");
+		return -1;		
+	}
+
+	if (getrtpsession(param->sess, param->recvPort) < 0)
+	{
+		printf("getrtpsession error \n");
+		return -1;
+	}
+
+	param->running = 1;
+	param->status = 0;
+	param->statusErrCnt = 0;
+	param->writefile = 1;
+
+	sendInvitePlay(&inst->gb28181Param, param, param->recvPort);
+
+	param->rtpthread = std::thread(jrtplib_rtp_recv_thread, (void*)param);
+
+	return 0;
+}
+
+int gb28181_stopstream(void *handle, char* deviceip)
+{
+	if (!handle || !deviceip)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+
+	CameraParams *param = NULL;
+	if (getdeviceinfo(inst, deviceip, &param) < 0)
+	{
+		printf("getdeviceinfo error \n");
+		return -1;
+	}
+
+	param->running = 0;
+	param->status = 0;
+	param->statusErrCnt = 0;
+	param->rtpthread.join();
+
+	sendPlayBye(inst, param);
+	return 0;
+}
+
+int gb28181_getregisterstatus(void *handle, char* deviceip)
+{
+	if (!handle || !deviceip)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+	CameraParams *param = NULL;
+	if (getdeviceinfo(inst, deviceip, &param) < 0)
+	{
+		printf("getdeviceinfo error \n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int gb28181_getinfo(void *handle, char* deviceip, int *width, int *height)
+{
+	if (!handle || !deviceip)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+	CameraParams *param = NULL;
+	if (getdeviceinfo(inst, deviceip, &param) < 0)
+	{
+		printf("getdeviceinfo error \n");
+		return -1;
+	}
+
+	if( !param->decoder.GetInfo(width, height) )
+	{
+		printf("GetRGBData error \n");
+		return -1;		
+	}
+
+	return 0;
+}
+
+int gb28181_getrgbdata(void *handle, char* deviceip, uint8_t *data, int width, int height)
+{
+	if (!handle || !deviceip)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+	CameraParams *param = NULL;
+	if (getdeviceinfo(inst, deviceip, &param) < 0)
+	{
+		printf("getdeviceinfo error \n");
+		return -1;
+	}
+
+	if( !param->decoder.GetRGBData(data, width, height) )
+	{
+//		printf("GetRGBData error \n");
+		return -1;		
+	}
+
+	return 0;
+}
+
+int gb28181_uninit(void *handle)
+{
+	if (!handle)
+		return -1;
+
+	liveVideoStreamParams *inst = (liveVideoStreamParams *)handle;
+	inst->gb28181Param.running = 0;
+	inst->gb28181Param.msgthread.join();
+
+	mysip_uninit(inst->gb28181Param.eCtx);
+	delete inst;
+
+	return 0;
+}
